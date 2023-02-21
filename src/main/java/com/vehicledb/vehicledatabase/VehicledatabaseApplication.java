1a0cdb29cee4fe2f7196f7fcd4bcfdafc30f5fb6
Ogada Moses <ogadamoses59@gmail.com>
2023-02-21 08:46:45 +0300

    Initialize Repository

---
+++ src/main/java/com/vehicledb/vehicledatabase/VehicledatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.vehicledb.vehicledatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicledatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicledatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vehicledb/vehicledatabase/VehicledatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicledb.vehicledatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicledatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
