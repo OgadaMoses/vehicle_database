8ca5b5e6e8b734185c793e3d06941cbcd31b133c
Ogada Moses <ogadamoses59@gmail.com>
2023-02-23 06:56:48 +0300

    Add H2 database configurations

--- src/main/java/com/vehicledb/vehicledatabase/domain/Vehicle.java
+++ src/main/java/com/vehicledb/vehicledatabase/domain/Vehicle.java
@@ -8,19 +8,27 @@ import javax.persistence.Id;
 @Entity
 
 public class Vehicle {
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
-	public Vehicle(String brand, String model, String color, String registerNumber, int year, int price) {
+	public Vehicle(String brand, String model, String color, String registerNumber, int vyear, int price) {
 		super();
 		this.brand = brand;
 		this.model = model;
 		this.color = color;
 		this.registerNumber = registerNumber;
-		this.year = year;
+		this.vyear = vyear;
 		this.price = price;
 	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
 	public String getBrand() {
 		return brand;
 	}
@@ -45,11 +53,12 @@ public class Vehicle {
 	public void setRegisterNumber(String registerNumber) {
 		this.registerNumber = registerNumber;
 	}
-	public int getYear() {
-		return year;
+	
+	public int getvYear() {
+		return vyear;
 	}
-	public void setYear(int year) {
-		this.year = year;
+	public void setvYear(int year) {
+		this.vyear = year;
 	}
 	public int getPrice() {
 		return price;
@@ -58,7 +67,5 @@ public class Vehicle {
 		this.price = price;
 	}
 	private String brand, model, color, registerNumber;
-	private int year, price; 
-	
-
+	private int vyear, price; 	
 }
---
+++ src/main/java/com/vehicledb/vehicledatabase/domain/VehicleRepository.java
@@ -0,0 +1,7 @@
+package com.vehicledb.vehicledatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VehicleRepository extends CrudRepository<Vehicle, Long> {
+
+}
