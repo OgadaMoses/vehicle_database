6ce7dc599dd45e36b8c339edec8e58f5c7187d1d
Ogada Moses <ogadamoses59@gmail.com>
2023-02-27 08:50:26 +0300

    Add Vehicle Owner Entity and Repository

---
+++ src/main/java/com/vehicledb/vehicledatabase/domain/OwnerRepository.java
@@ -0,0 +1,7 @@
+package com.vehicledb.vehicledatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+}
--- src/main/java/com/vehicledb/vehicledatabase/domain/VehicleRepository.java
+++ src/main/java/com/vehicledb/vehicledatabase/domain/VehicleRepository.java
@@ -1,7 +1,19 @@
 package com.vehicledb.vehicledatabase.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
-public interface VehicleRepository extends CrudRepository<Vehicle, Long> {
 
+
+
+public interface VehicleRepository extends CrudRepository<Vehicle, Long> {
+	// Fetch vehicles by brand
+	List<Vehicle> findByBrandAndModel(String brand, String model);
+    
+	// Find Vehicles by color
+	List<Vehicle> findByColor(String color);
+	
+	// Find Vehicles by year 
+	List<Vehicle> findByvyear (int vyear);
 }
