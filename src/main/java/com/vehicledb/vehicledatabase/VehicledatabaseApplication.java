abc934547855f99b2e4db0d381d5b718e42e393e
Ogada Moses <ogadamoses59@gmail.com>
2023-03-03 08:03:44 +0300

    add Spring security

--- src/main/java/com/vehicledb/vehicledatabase/VehicledatabaseApplication.java
+++ src/main/java/com/vehicledb/vehicledatabase/VehicledatabaseApplication.java
@@ -37,13 +37,18 @@ public class VehicledatabaseApplication implements CommandLineRunner {
 		Owner owner1 = new Owner ("Moses", "Ogada");
 		Owner owner2 = new Owner ("Moses", "Ogada");
 		Owner owner3 = new Owner ("Moses", "Ogada");
-		orepository.saveAll(Arrays.asList(owner1,owner2, owner3));
+		Owner owner4 = new Owner ("Musa", "Otieno");
+		Owner owner5 = new Owner ("Mose", "Ogada");
+		orepository.saveAll(Arrays.asList(owner1,owner2, owner3, owner4, owner5));
 		
 		Vehicle vehicle1 = new Vehicle("Toyota", "Harrier", "White", "H-2023", 2023, 3000000, owner1);
 		Vehicle vehicle2 = new Vehicle("Nissan", "CRV","White", "C-2023", 2023, 340000, owner2);
 		Vehicle vehicle3 = new Vehicle("Nissan", "TIIDA", "Cream", "T-2023", 2023, 1900000, owner3);
+		Vehicle vehicle4 = new Vehicle("Toyota", "Prado","White", "P-2023", 2023, 340000, owner2);
+		Vehicle vehicle5 = new Vehicle("Honda", "TCX-5", "Cream", "C-2023", 2023, 1900000, owner3);
 		
-		repository.saveAll(Arrays.asList (vehicle1,vehicle2,vehicle3));
+		
+		repository.saveAll(Arrays.asList (vehicle1,vehicle2,vehicle3, vehicle4,vehicle5));
 		
 		for (Vehicle vehicle :repository.findAll()) {
 			logger.info(vehicle.getBrand () + " " +
--- src/main/java/com/vehicledb/vehicledatabase/domain/VehicleRepository.java
+++ src/main/java/com/vehicledb/vehicledatabase/domain/VehicleRepository.java
@@ -3,16 +3,18 @@ package com.vehicledb.vehicledatabase.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 
-
+@RepositoryRestResource
 public interface VehicleRepository extends CrudRepository<Vehicle, Long> {
 	// Fetch vehicles by brand
-	List<Vehicle> findByBrandAndModel(String brand, String model);
+	List<Vehicle> findByBrandAndModel(@Param ("brand") String brand, @Param("model") String model);
     
 	// Find Vehicles by color
-	List<Vehicle> findByColor(String color);
+	List<Vehicle> findByColor(@Param ("color") String color);
 	
 	// Find Vehicles by year 
 	List<Vehicle> findByvyear (int vyear);
